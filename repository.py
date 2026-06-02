33f390bac0fbc765e64b9402b1f7cfc7b16371e5
briankntro30-lang <briankntro30@gmail.com>
2026-06-02 14:52:55 +0200

    Update repository.py

--- repository.py
+++ repository.py
@@ -1,5 +1,6 @@
 from database import get_connection
 
+
 def save_simogramme(meta, kpis):
 
     conn = get_connection()
@@ -7,21 +8,31 @@ def save_simogramme(meta, kpis):
 
     cur.execute("""
         INSERT INTO simogrammes (
-            reference, machine, pdc, date,
-            temps_cycle, taux_machine,
-            taux_operateur, pieces_heure, pieces_jour
+            reference,
+            machine,
+            pdc,
+            date,
+            temps_cycle,
+            temps_machine,
+            temps_operateur,
+            temps_attente,
+            taux_homme,
+            taux_machine,
+            pieces_jour
         )
-        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
     """, (
         meta["reference"],
         meta["machine"],
         meta["pdc"],
         meta["date"],
-        kpis["temps_cycle"],
-        kpis["taux_machine"],
-        kpis["taux_operateur"],
-        kpis["pieces_heure"],
-        kpis["pieces_jour"]
+        kpis.get("temps_cycle", 0),
+        kpis.get("temps_machine", 0),
+        kpis.get("temps_operateur", 0),
+        kpis.get("temps_attente", 0),
+        kpis.get("taux_homme", 0),
+        kpis.get("taux_machine", 0),
+        kpis.get("pieces_jour", 0)
     ))
 
     conn.commit()
@@ -34,7 +45,9 @@ def get_simogrammes():
     cur = conn.cursor()
 
     cur.execute("SELECT * FROM simogrammes")
+
     data = cur.fetchall()
 
     conn.close()
+
     return data
