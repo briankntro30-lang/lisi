cf815d0fd1163959bd139dbaf2399caf17a37ba0
briankntro30-lang <briankntro30@gmail.com>
2026-06-02 14:36:57 +0200

    Update login.py

--- login.py
+++ login.py
@@ -2,12 +2,12 @@ import streamlit as st
 
 def login_page():
 
-    st.markdown("## Connexion")
+    st.markdown("## Connexion - Simogramme")
 
     user = st.text_input("Utilisateur")
     pwd = st.text_input("Mot de passe", type="password")
 
-    if st.button("Login"):
+    if st.button("Se connecter"):
         if user == "admin" and pwd == "1234":
             st.session_state["logged_in"] = True
             st.rerun()
